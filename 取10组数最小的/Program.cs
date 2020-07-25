537e2793a7e2334b916ef4d135824feb0c5cc637
Lind <bfyxzls@sina.com>
2020-07-25 13:32:53 +0800

    添加项目文件。

---
+++ 取10组数最小的/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 取10组数最小的
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
