dd8b8d7110645f6683453dcc1616135b7a10f6d3
Lind <bfyxzls@sina.com>
2020-07-26 20:30:09 +0800

    init

---
+++ 取10组数最小的/Lottery.cs
@@ -0,0 +1,27 @@
+﻿namespace 取10组数最小的
+{
+    /// <summary>
+    /// 彩票
+    /// </summary>
+    class Lottery
+    {
+        public int Number { get; set; }
+        public decimal Amount { get; set; }
+        public int row { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            Lottery lottery = (Lottery)obj;
+            return base.Equals(lottery.Amount.Equals(this.Amount) && lottery.Number.Equals(this.Number));
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return "{number=" + Number + ",amount=" + Amount + ",row=" + row + "}";
+        }
+    }
+}
--- 取10组数最小的/Program.cs
+++ 取10组数最小的/Program.cs
@@ -1,15 +1,184 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace 取10组数最小的
 {
     class Program
     {
         static void Main(string[] args)
         {
+            List<List<Lottery>> oldLotteryList = JsonConvert.DeserializeObject<List<List<Lottery>>>(File.ReadAllText("test.json"));
+
+            //  List<List<Lottery>> lotteryList = new List<List<Lottery>>();
+            //  Init(lotteryList);
+            for (int i = 0; i < 10; i++)
+            {//一次比较，从1号到10位位，位置交替，从1到10，1走完放到最后，2开始走。。。
+                List<List<Lottery>> lotteryList = JsonConvert.DeserializeObject<List<List<Lottery>>>(File.ReadAllText("test.json"));
+                
+
+                DisplayAll("原数据", lotteryList);
+                Compare(lotteryList);
+                DisplayAll("排序和比较", lotteryList);
+                Repeat(lotteryList);
+                DisplayAll("位置去重复", lotteryList);
+                var result = DicToList(lotteryList);
+                Display("结果", result);
+            }
+
+
+
+            Console.ReadKey();
+        }
+        /// <summary>
+        ///  做比较
+        /// </summary>
+        /// <param name="lotteryList"></param>
+        static void Compare(List<List<Lottery>> lotteryList)
+        {
+            for (int i = 0; i < lotteryList.Count; i++)
+            {
+                lotteryList[i] = lotteryList[i].OrderBy(o => o.Amount).ToList();
+            }
+
+            for (int i = 0; i < lotteryList.Count - 1; i++)
+            {
+
+                for (int j = i + 1; j < lotteryList.Count; j++)
+                {
+                    //最小值相同位置比较
+                    if (lotteryList[i][0].Number == lotteryList[j][0].Number && lotteryList[i][0].Amount > lotteryList[j][0].Amount) //位置相同，两个数字可能换位
+                    {
+                        if (lotteryList[i][1].Amount > lotteryList[j][1].Amount)
+                        {
+                            var lottery = lotteryList[i][0];
+                            lotteryList[i][0] = lotteryList[i][1];
+                            lotteryList[i][1] = lottery;
+                        }
+                        else
+                        {
+                            var lottery = lotteryList[j][0];
+                            lotteryList[j][0] = lotteryList[j][1];
+                            lotteryList[j][1] = lottery;
+                        }
+
+                    }
+
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// 位置去重复
+        /// </summary>
+        /// <param name="lotteryList"></param>
+        static void Repeat(List<List<Lottery>> lotteryList)
+        {
+
+            bool repeat = false;
+            for (int i = 0; i < lotteryList.Count - 1; i++)
+            {
+                for (int j = i + 1; j < lotteryList.Count; j++)
+                {
+                    //最小值相同位置比较
+                    if (lotteryList[i][0].Number == lotteryList[j][0].Number) //位置相同，两个数字可能换位
+                    {
+                        repeat = true;
+                        if (lotteryList[i][0].Amount > lotteryList[j][0].Amount)//第二小的元素也比第二行的大，这时取第二行的数字
+                        {
+                            lotteryList[i].RemoveAt(0);//移除，因为位置不能重复
+                            lotteryList[i].Add(lotteryList[j][0]);
+                        }
+                        else
+                        {
+                            lotteryList[j].RemoveAt(0);
+                            lotteryList[j].Add(lotteryList[j][0]);
+
+                        }
+                    }
+
+                }
+            }
+            if (repeat)
+            {
+                Repeat(lotteryList);
+            }
+
+        }
+
+        static List<Lottery> DicToList(List<List<Lottery>> lotteryList)
+        {
+            List<Lottery> lotteries = new List<Lottery>();
+            lotteryList.ForEach(o =>
+            {
+                lotteries.Add(o[0]);
+            });
+            return lotteries;
+        }
+
+        /// <summary>
+        /// 比较最小低，打印结果
+        /// </summary>
+        /// <param name="lotteryList"></param>
+        static void Display(string message, List<Lottery> lotteries)
+        {
+            Console.WriteLine("----------------------------{0}----------------------------", message);
+
+            decimal total = 0m;
+
+            //打印结果
+            lotteries.ToList().ForEach(i =>
+            {
+                Console.WriteLine("{2} {0}[{1}] ", i.Number, i.Amount, i.row);
+                total += i.Amount;
+            });
+            Console.WriteLine("合计:{0}", total);
+
+        }
+        static void DisplayAll(string message, List<List<Lottery>> lotteries)
+        {
+            Console.WriteLine("----------------------{0}--------------------------------", message);
+            for (int j = 0; j < 10; j++)
+            {
+                foreach (var res in lotteries[j])
+                {
+                    Console.Write("" + res.Number + "[" + res.Amount + "]  ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        /// <param name="lotteryList"></param>
+        static void Init(List<List<Lottery>> lotteryList)
+        {
+            for (int j = 1; j <= 10; j++)
+            {
+                List<Lottery> lotteries = new List<Lottery>();
+                for (int i = 1; i <= 10; i++)
+                {
+                    int res = GetRandomCode();
+                    Console.Write("" + i + "[" + res + "]  ");
+                    lotteries.Add(new Lottery() { Amount = res, Number = i, row = j });
+                }
+                Console.WriteLine();
+                lotteryList.Add(lotteries);
+            }
+            String file = JsonConvert.SerializeObject(lotteryList);
+        }
+
+        static int GetRandomCode()
+        {
+            byte[] bytes = new byte[4];
+            System.Security.Cryptography.RNGCryptoServiceProvider rng = new System.Security.Cryptography.RNGCryptoServiceProvider();
+            rng.GetBytes(bytes);
+            int res = BitConverter.ToInt32(bytes, 0);
+            Random random = new Random(res);
+            return random.Next(10, 99);
         }
     }
 }
